2ad6262fecacf021d92feeef5e9fd8273f28e5e9
WENSON GAN <wgan@edu.uwaterloo.ca>
2020-09-21 14:45:59 -0400

    Fixing bugs in dealloc and first fit count exit frag, dealt with general case in dealloc and fixed while loop in count ext frag

--- SVC/src/mem.c
+++ SVC/src/mem.c
@@ -143,29 +143,57 @@ void first_fit_dealloc(void *ptr) {
     node_t *deallocateNode = (ptr - sizeOf(deallocateNode));
     void *startingAddress= ptr - sizeOf(deallocateNode);
     void *endingAddress = ptr + deallocateNode->size;
-
     node_t *curNode = free_mem_head;
 
+    int frontSet =FALSE;
+    node_t *nodeBefore=NULL;
 
-
+    //Iterate through unallocated data linked list
     while(curNode!=NULL){
         
         //if the end of current Node's address space is the starting address expand data
         //Cur node is starting address
         //increment sizeOf(node_t) to increment past it
         //Add the curNode->size +1 to get to end of size and to next data segment
-        if(((void*) curNode + sizeOf(curNode) + curNode->size + 1 )== startingAddress ){
+        if(((void*) curNode + sizeOf(curNode) + curNode->size + 4 )== startingAddress ){
             curNode->next = deallocateNode->next;
             curNode->size= curNode->size+sizeOf(deallocateNode)+deallocateNode->size;
-            //This line below is so we can keep track of the deallocated node 
+            //This line below is so we can keep track of the deallocated node as it now an extension of curNode
             //if we have Free|Dealocated|Free memory situation
             deallocateNode= curNode;
+            frontSet = TRUE;
         }
-        else if((void*) curNode = endingAddress+4){
+        else if((void*) curNode == endingAddress+4){
             deallocateNode->next = curNode->next;
             deallocateNode->size = deallocateNode->size +sizeOf(curNode)+curNode->size;
         }
+        
+        //Find the previous free node in linked list before the deallocateNode
+        if(curNode<startingAddress){
+            nodeBefore = curNode;
+        }
+        
         curNode = curNode->next;
+    }
+
+    //Put the deallocated node in the correct location within the linked list of unallocated data
+    //Assuming we have the case allocated|Deallocate|(allocated or Free) 
+    if(!frontSet){
+        deallocateNode->prev = nodeBefore;
+        if(nodeBefore->next < endingAddress){
+            print("We have encountered an issue in allocated|Deallocate|(allocated or free) Code error 1");
+        }
+        else if(nodeBefore > startingAddress){
+            print("We have encountered an issue in allocated|Deallocate|(allocated or free) Code error 2");
+        }
+        else if (nodeBefore+nodeBefore->size+sizeOfNode(nodeBefore)>startingAddress){
+            print("We have encountered an issue in allocated|Deallocate|(allocated or free) Code error 3");
+        }
+        else if (nodeBefore+nodeBefore->size+sizeOfNode(nodeBefore)+4==startingAddress){
+            print("We have encountered an issue in allocated|Deallocate|(allocated or free) Code error 4");
+        }
+        deallocateNode->next = nodeBefore->next;
+        nodeBefore->next = deallocateNode;
     }
 
     return;
@@ -176,9 +204,10 @@ int first_fit_count_extfrag(size_t size) {
     node_t *curNode = free_mem_head;
 
     while(curNode!=NULL){
-        if (curNode->size<size){
+        if (curNode->size<=size){
             counter++;
         }
+        curNode = curNode->next;
     }
     return counter;
 }
