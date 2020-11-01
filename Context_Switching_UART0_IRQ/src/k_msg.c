bee408626301e41fe476a907965fbdeb96e16968
Varan Narendran <varannaren@Varans-MacBook-Pro.local>
2020-11-01 18:44:01 -0500

    first commit for recv_msg

--- Context_Switching_UART0_IRQ/src/k_msg.c
+++ Context_Switching_UART0_IRQ/src/k_msg.c
@@ -6,6 +6,8 @@
  */
 
 #include "k_msg.h"
+#include "k_task.h"
+#include "k_mem.h"
 
 #ifdef DEBUG_0
 #include "printf.h"
@@ -26,9 +28,46 @@ int k_send_msg(task_t receiver_tid, const void *buf) {
 }
 
 int k_recv_msg(task_t *sender_tid, void *buf, size_t len) {
-#ifdef DEBUG_0
-    printf("k_recv_msg: sender_tid  = 0x%x, buf=0x%x, len=%d\r\n", sender_tid, buf, len);
-#endif /* DEBUG_0 */
+    #ifdef DEBUG_0
+        printf("k_recv_msg: sender_tid  = 0x%x, buf=0x%x, len=%d\r\n", sender_tid, buf, len);
+    #endif /* DEBUG_0 */
+    if ( !(len > 0)) {
+        return RTX_ERR;
+    }
+
+    //trap into kernel- atomicity on / disable interrupts
+    __disable_irq();
+    TCB* curr_task = gp_current_task;
+
+    if (!curr_task->mailbox)
+        return RTX_ERR;
+    
+    while(is_empty(curr_task->mailbox))
+    {
+        curr_task->state = BLK_MSG;
+        k_tsk_yield();
+    }
+
+    buf = k_mem_alloc(len);
+    void* ptr;
+    //unsure how dequeueing will be implemented for cicurlar queue
+    dequeue_msg(curr_task->mailbox, ptr);
+
+    if (ptr == NULL)
+        return RTX_ERR;
+    //check if len < size of the message - length field should be first 4 bytes of the message (in the header)
+    int actual_len;
+    memcpy(&actual_len, ptr, 4);
+    if (len < actual_len)
+        return RTX_ERR;
+
+    memcpy(buf, ptr, len);
+    //*sender_tid = ; //??
+    //^---SENDER TID NEEDS TO BE SAVED SOMEWHERE IN THE MESSAGE, can't change message header, put it right after header??
+
+    //atomicity off / enable interrupts
+    __enable_irq();
+
     return 0;
 }
 
