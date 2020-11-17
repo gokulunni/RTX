3245b76625bb5d04fe72db0cd4b2448f8aea2505
Wenson Gan <wgan@uwaterloo.ca>
2020-11-17 15:33:30 -0500

    updated time k_time files

--- Real_Time_Services/src/k_time.c
+++ Real_Time_Services/src/k_time.c
@@ -1,44 +1,20 @@
 /**
- * @file:   k_mem.c
- * @brief:  kernel memory managment routines
+ * @file:   k_time.c
+ * @brief:  timing routines
  * @author: Yiqing Huang
  * @date:   2020/09/28
  */
 
-#include "k_mem.h"
 #include "common.h"
 #include "uart_polling.h"
 #ifdef DEBUG_MEM
 #include "printf.h"
 #endif /* ! DEBUG_MEM */
-#define CEIL(x,y) (((x)+(y)-1)/(y))
 
-/* The final memory map of IRAM1 looks like the following. 
-   Refer to k_task.c for initilization function details.
----------------------------------------------------------------------------
-
-                    0x10008000+---------------------------+ High Address
-                              |                           |
-                              |  Free Memory Space        |
-                              |                           |
-                              |                           |
-                              |                           |
-                              |---------------------------|
-                              |        Padding            |
-&Image$$RW_IRAM1$$ZI$$Limit-->|---------------------------|
-                              |                           |          
-                              |       RTX  Image          |
-                              |                           |
-                    0x10000000+---------------------------+ Low Address
-
-*/
 
 
 int get_time(struct timeval_rt *tv);
 
-
-extern unsigned int Image$$RW_IRAM1$$ZI$$Limit;
-extern TCB *gp_current_task;
 
 int k_get_time(struct timeval_rt *tv){
     U32 end_addr;
@@ -53,8 +29,6 @@ int k_get_time(struct timeval_rt *tv){
         #endif /* DEBUG_MEM */
         return RTX_ERR;
     }
-
-    //end_addr = (unsigned int) &Image$$RW_IRAM1$$ZI$$Limit;
     
     return get_time(tv);
 }
@@ -62,7 +36,7 @@ int k_get_time(struct timeval_rt *tv){
 
 
 /*
-*  First Fit Memory Allocation
+*  get time function
 */
 
 
--- Real_Time_Services/src/k_time.h
+++ Real_Time_Services/src/k_time.h
@@ -1,6 +1,6 @@
 /**
- * @file:   k_mem.h
- * @brief:  kernel memory managment header file
+ * @file:   k_time.h
+ * @brief:  kernel timer managment header file
  * @author: Yiqing Huang
  * @date:   2020/09/20
  */
@@ -9,20 +9,7 @@
 #define K_MEM_H_
 
 #include "k_rtx.h"
-
-/* ----- Definitions ----- */
-#define IRAM1_END 0x10008000
-
-/* ----- Variables ----- */
-/* This symbol is defined by linker (see ARM Linker User Guide in Arm Compiler 5 Documentation) */   
-extern U32 Image$$RW_IRAM1$$ZI$$Limit;
-
 /* ----- Functions ------ */
-int k_mem_init(size_t blk_size, int algo);
-void *k_mem_alloc(size_t size);
-int k_mem_dealloc(void *ptr);
-int k_mem_count_extfrag(size_t size);
+int k_get_time(size_t blk_size, int algo);
 
-int mem_cpy(void *destination, void *source, size_t size);
 
-#endif /* ! K_MEM_H_ */
