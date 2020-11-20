cc168de854878fb799cc14bdb04db6fd4b4083a3
Gokul Unnikrishnan <g2999unn@uwaterloo.ca>
2020-11-19 23:21:34 -0500

    modified command registration in kcd

--- Real_Time_Services/src/kcd_task.h
+++ Real_Time_Services/src/kcd_task.h
@@ -3,14 +3,14 @@
 #include "rtx.h"
 
 typedef struct registered_command {
-	char *cmd;
+	char cmd;
 	task_t handler_tid;
 	struct registered_command *next;
 }REGISTERED_CMD_T;
 
 void push_cmd(REGISTERED_CMD_T **registered_cmd_head, REGISTERED_CMD_T *new_cmd);
 //REGISTERED_CMD_T *pop_cmd(REGISTERED_CMD_T **registered_cmd_head);
-REGISTERED_CMD_T *get_cmd(REGISTERED_CMD_T *registered_cmd_head, char *cmd);
+REGISTERED_CMD_T *get_cmd(REGISTERED_CMD_T *registered_cmd_head, char cmd);
 
 int str_cmp(const char *str1, const char *str2);
 
