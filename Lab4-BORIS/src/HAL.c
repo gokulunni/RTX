488d05fb15e725500ec61046ea12f97524dc24c5
boris_nguyen <dnnguyen@edu.uwaterloo.ca>
2020-11-22 07:38:43 -0500

    Hal.c

--- Lab4-BORIS/src/HAL.c
+++ Lab4-BORIS/src/HAL.c
@@ -4,14 +4,16 @@
  * NOTE: This file contains embedded assembly. 
  *       The code borrowed some ideas from ARM RL-RTX source code
  */
- 
+
+#include "k_rtx.h"
+
+extern TCB *gp_current_task;
+
 /* pop off exception stack frame from the stack */
 __asm void __rte(void)
 {
   PRESERVE8            ; 8 bytes alignement of the stack
-  MVN  LR, #:NOT:0xFFFFFFF9  ; set EXC_RETURN value, Thread mode, MSP
-  CPSIE I              ; enable interrupt
-  BX   LR
+  B SVC_EXIT           ; Exit SVC_Handler
 }
 
 /* NOTE: assuming MSP is used. Ideally, PSP should be used */
@@ -19,9 +21,19 @@ __asm void SVC_Handler (void)
 {
   PRESERVE8            ; 8 bytes alignement of the stack
   CPSID I              ; disable interrupt
+  CMP LR, 0xFFFFFFF9   ; Check LR value to see if MSP or PSP was used
+  BEQ SET_MSP_SP       ; if 1, handler was invoked by kernel thread
+  B SET_PSP_SP         ; if 0, handler was invoked by user thread
+
+SET_MSP_SP
   MRS  R0, MSP         ; Read MSP
-	
-  
+  B NORMAL_OPERATION
+
+SET_PSP_SP
+  MRS  R0, PSP         ; Read PSP
+  B NORMAL_OPERATION
+
+NORMAL_OPERATION
   LDR  R1, [R0, #24]   ; Read Saved PC from SP
                        ; Loads R1 from a word 24 bytes above the address in R0
                        ; Note that R0 now contains the the SP value after the
@@ -45,9 +57,27 @@ __asm void SVC_Handler (void)
   MRS  R12, MSP        ; Read MSP
   STR  R0, [R12]       ; store C kernel function return value in R0
                        ; to R0 on the exception stack frame  
-SVC_EXIT  
-	
-  MVN  LR, #:NOT:0xFFFFFFF9  ; set EXC_RETURN value, Thread mode, MSP
-  CPSIE I              ; enable interrupt
+
+SVC_EXIT
+  LDR R3, =__cpp(&gp_current_task)    ; Load R3 with address of pointer to current task
+  LDR R3, [R3]                        ; Get address of current task
+  MOV R2, #0                          ; clear R2
+  LDRB R2, [R3, #45]                  ; read priv member (45 byte offset)
+  CMP R2, #1                          ; check if priv level is 1 or 0
+  BEQ kernel_thread                   ; if 1, handler was invoked by kernel thread
+  B user_thread                       ; if 0, handler was invoked by user thread
+
+kernel_thread
+  MVN LR, #:NOT:0xFFFFFFF9   ; set EXC_RETURN value to Thread mode, MSP
+  MOV R3, #0                 ;
+  MSR CONTROL, R3            ; set control bit[0] to 0 (privileged)
+  CPSIE I                    ; enable interrupt
+  BX   LR
+
+user_thread
+  MVN LR, #:NOT:0xFFFFFFFD   ; set EXC_RETURN value to Thread mode, PSP
+  MOV R3, #1                 ;
+  MSR CONTROL, R3            ; set control bit[0] to 1 (unpriviledged)
+  CPSIE I                    ; enable interrupt
   BX   LR
 }
