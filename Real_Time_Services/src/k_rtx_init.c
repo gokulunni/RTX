fd8ba28b4d4586ee08539a329956f70f7cc62152
Wenson Gan <wgan@uwaterloo.ca>
2020-11-22 17:07:25 -0500

    adding timer 1

--- Real_Time_Services/src/k_rtx_init.c
+++ Real_Time_Services/src/k_rtx_init.c
@@ -31,6 +31,9 @@ int k_rtx_init(size_t blk_size, int algo, RTX_TASK_INFO *task_info, int num_task
 	if(timer_init(0)!=RTX_OK){
 		return RTX_ERR;
 	}
+	if(timer_init_100MHZ(1)!=RTX_OK){
+		return RTX_ERR;
+	}
 			
     
     /* start the first task */
