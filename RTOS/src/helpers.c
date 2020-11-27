8eff707cd2cb7479c6b0a1755f77046cb2200fe6
boris_nguyen <dnnguyen@edu.uwaterloo.ca>
2020-11-27 04:29:16 -0500

    Helper functions

---
+++ RTOS/src/helpers.c
@@ -0,0 +1,36 @@
+//
+// Created by Boris Nguyen on 2020-11-22.
+//
+
+#include "k_mem.h"
+#include "common.h"
+
+int mem_cpy(void *destination, void *source, size_t size) {
+    if (destination == NULL || source == NULL) {
+        return RTX_ERR;
+    }
+
+    if (size <= 0) {
+        return RTX_ERR;
+    }
+
+    char *dest = destination;
+    char *src = source;
+    for (int i = 0; i < size; i++) {
+        dest[i] = src[i];
+    }
+
+    return RTX_OK;
+}
+
+int str_cmp(const char *str1, const char *str2) {
+    int s1;
+    int s2;
+    do {
+        s1 = *str1++;
+        s2 = *str2++;
+        if (s1 == 0)
+            break;
+    } while (s1 == s2);
+    return (s1 != s2) ? RTX_ERR : RTX_OK;
+}
---
+++ RTOS/src/helpers.h
@@ -0,0 +1,11 @@
+//
+// Created by Boris Nguyen on 2020-11-22.
+//
+
+#ifndef ECE350_HELPERS_H
+#define ECE350_HELPERS_H
+
+int mem_cpy(void *destination, void *source, size_t size);
+int str_cmp(const char *str1, const char *str2);
+
+#endif //ECE350_HELPERS_H
