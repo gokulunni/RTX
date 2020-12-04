a844e0b41efedd6879cd111739286dac8179f792
Wenson Gan <wgan@uwaterloo.ca>
2020-12-04 14:17:19 -0500

    adding function implementation

--- RTOS/src/k_rtx_init.c
+++ RTOS/src/k_rtx_init.c
@@ -12,13 +12,59 @@
 #include "k_task.h"
 #include "timer.h"
 
-int rtx_init_rt(RTX_SYS_INFO *sys_info, RTX_TASK_INFO *tasks, int num_tasks){
-	
-	
+RTX_SYS_INFO sys_info_copy;
+int sys_info_set=0;
+
+int k_rtx_init_rt(RTX_SYS_INFO *sys_info, RTX_TASK_INFO *tasks, int num_tasks){
+		
+		/* interrupts are already disabled when we enter here */
+    if (uart_irq_init(0) != RTX_OK) {
+        return RTX_ERR;
+    }
+    
+    if (k_mem_init(sys_info->mem_blk_size, sys_info->mem_algo) != RTX_OK) {
+        return RTX_ERR;
+    }
+
+    if (k_tsk_init(tasks, num_tasks) != RTX_OK ) {
+        return RTX_ERR;
+    }
+
+    if (timer_init(0) != RTX_OK){
+        return RTX_ERR;
+    }
+
+    if (timer_init_100MHZ(1) != RTX_OK) {
+        return RTX_ERR;
+    }
+		
+		//save sys_info
+		sys_info_copy.mem_blk_size = sys_info->mem_blk_size;
+		sys_info_copy.mem_algo = sys_info->mem_algo;
+		sys_info_copy.rtx_time_qtm = sys_info->rtx_time_qtm;
+		sys_info_copy.server = sys_info->server;
+		sys_info_copy.sched = sys_info->sched;
+		sys_info_set=1;
+		
+    
+    /* start the first task */
+    return k_tsk_yield();
 }
 
-int get_sys_info(RTX_SYS_INFO *buffer){
+
+int k_get_sys_info(RTX_SYS_INFO *buffer){
 	
+		if (buffer!=NULL){
+			return 0;
+		}
+		
+		buffer->mem_blk_size =sys_info_copy.mem_blk_size;
+		buffer->mem_algo=sys_info_copy.mem_algo;
+		buffer->rtx_time_qtm=sys_info_copy.rtx_time_qtm;
+		buffer->server=sys_info_copy.server;
+		buffer->sched=sys_info_copy.sched;
+	
+		return sys_info_set;
 }
 
 int k_rtx_init(size_t blk_size, int algo, RTX_TASK_INFO *task_info, int num_tasks)
